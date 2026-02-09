ebc7946c7a07e1feb5507675218046b68e12df2a
Tulsa Daley <tulsadaley@gmail.com>
2026-02-09 20:35:05 +1100

    fixed landing page responsive issue

--- app/routes/landing.tsx
+++ app/routes/landing.tsx
@@ -109,23 +109,21 @@ export default function Landing() {
               An experienced designer who thinks in systems, loves visual craft,
               and is getting comfy in code.
             </p>
-            <p className="flex items-center gap-2 text-sm text-gray-500 font-mono">
-              <span className="shrink-0">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="w-4 h-4"
-                  aria-hidden
-                >
-                  <title>Location</title>
-                  <path
-                    fillRule="evenodd"
-                    d="M11.54 22.351l.07.04.028.016a.76.76 0 00.723 0l.028-.015.071-.041a16.975 16.975 0 001.144-.742 19.58 19.58 0 002.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 00-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 002.682 2.282 16.975 16.975 0 001.145.742zM12 13.5a3 3 0 100-6 3 3 0 000 6z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </span>
+            <p className="text-sm text-gray-500 font-mono leading-relaxed">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-4 h-4 inline-block align-text-bottom mr-1.5 shrink-0"
+                aria-hidden
+              >
+                <title>Location</title>
+                <path
+                  fillRule="evenodd"
+                  d="M11.54 22.351l.07.04.028.016a.76.76 0 00.723 0l.028-.015.071-.041a16.975 16.975 0 001.144-.742 19.58 19.58 0 002.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 00-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 002.682 2.282 16.975 16.975 0 001.145.742zM12 13.5a3 3 0 100-6 3 3 0 000 6z"
+                  clipRule="evenodd"
+                />
+              </svg>
               Currently doing Design Systems at{" "}
               <a
                 href="https://www.cargowise.com/"
