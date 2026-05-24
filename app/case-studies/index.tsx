8558c4ed067052fba48cb61563688932c7a7bec3
Michael Hudson <huddo121@gmail.com>
2026-05-24 10:42:46 +1000

    Set up the before and after slider for the 'Core framework redesign' project

--- app/case-studies/index.tsx
+++ app/case-studies/index.tsx
@@ -1,5 +1,6 @@
 import type { Project } from "../data/projects";
 import type { ReactElement } from "react";
+import { FrameworkRedesignCaseStudy } from "./framework-redesign";
 import { IconsCaseStudy } from "./icons";
 import { MultiLevelGroupingCaseStudy } from "./multi-level-grouping";
 
@@ -9,6 +10,7 @@ export type CaseStudyComponent = (props: { project: Project }) => ReactElement;
 export const caseStudyComponents: Record<number, CaseStudyComponent> = {
   1: IconsCaseStudy,
   4: MultiLevelGroupingCaseStudy,
+  5: FrameworkRedesignCaseStudy,
   // etc.
 };
 
